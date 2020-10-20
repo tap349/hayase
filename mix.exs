d0b6fce211d1646f2b2cf70efdffdbb2c37c4209
Alexey Terekhov <alexey.terekhov.tap349@gmail.com>
2020-10-20 11:55:46 +0300

    bump version from 0.1.2 to 0.1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Hayase.MixProject do
   def project do
     [
       app: :hayase,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
