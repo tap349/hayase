9ecb54c26acb0e5dc0dd29fb4d6b475bf5a6c977
Alexey Terekhov <alexey.terekhov.tap349@gmail.com>
2021-06-08 09:21:46 +0300

    Use M and F aliases

--- lib/hayase.ex
+++ lib/hayase.ex
@@ -1,8 +1,8 @@
 defmodule Hayase do
   defmacro __using__(_opts) do
     quote do
-      alias Hayase.Typeclasses.Functor
-      alias Hayase.Typeclasses.Monad
+      alias Hayase.Typeclasses.Functor, as: F
+      alias Hayase.Typeclasses.Monad, as: M
       alias Hayase.Types.{Maybe, Result}
     end
   end
